043d28b98748a89b4bf28f3fb0f1be7acce75a18
Michael Kubica <kubicamh@gmail.com>
2017-09-26 16:17:57 -0400

    Lab 5
    Added Lab 5 for Java Script

---
+++ labs/main05.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+	document.getElementById('hoursButton').addEventListener('click', runCheckHours);
+};
+
+
+function runCheckHours () {
+	var hoursSlept = document.getElementById('hoursInput').value;
+	checkHours(parseFloat(hoursSlept));
+
+	function checkHours(numHours) {
+		if (hoursSlept >= 8) {
+			return alert("You're getting too much sleep!");
+		} else {
+			return alert("Get some more sleep!");
+		}
+	};
+
+};
+
